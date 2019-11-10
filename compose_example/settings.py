f7c90959402d684fc5d97c2554e71c9ae458eb1b
wudinglu <xuzhen0913@163.com>
2019-11-10 22:15:44 +0800

    init

--- compose_example/settings.py
+++ compose_example/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w^o0(ba%c&ny=^5(-%yqgzcl9*ewj4igl9lu(my!7)_e%j$uv$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", ]
 
 
 # Application definition
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'compose_example.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
